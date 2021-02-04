6380992f4d6cd76395995ef2e6daa6d73395d761
Carlos Almeida <cosmosdesigner@gmail.com>
2021-02-04 12:33:24 +0000

    Change page title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 
